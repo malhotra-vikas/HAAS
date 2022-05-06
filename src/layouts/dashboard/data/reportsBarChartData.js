d43e53fb006900ecde5fb11249df2b453d180e29
Vikas <malhotra.vikas@gmail.com>
2022-05-05 22:48:20 -0400

    1st version

---
+++ src/layouts/dashboard/data/reportsBarChartData.js
@@ -0,0 +1,19 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+Vikas was here
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+export default {
+  labels: ["M", "T", "W", "T", "F", "S", "S"],
+  datasets: { label: "Interviews", data: [50, 20, 10, 22, 50, 5, 0] },
+};
